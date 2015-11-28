1a1dc7b8b52ead377cece495dbe457d6f2aa3f1a
imox2 <talhaanwar.anwar@gmail.com>
2015-11-28 11:50:05 +0530

    modified script and its doc

--- circular_prime.py
+++ circular_prime.py
@@ -4,15 +4,18 @@ The number, 197, is called a circular prime because all rotations of the digits:
 
 There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
 
+How many circular are there below one million?
+
 '''
 
-import math
+import math #to find square root of a number
 
 l=[]
 circ=[]
 st=''
+temp1=[]
 
-def check_prime(n):
+def check_prime(n): 
 	n=float(n)
 	sqr=math.sqrt(n)
 	#print(sqr)
@@ -31,18 +34,18 @@ def check_prime(n):
 		return 0
 
 
-for inp in range(1,101):
+for inp in range(2,1000001): #stores all prime till range(2,n) in a list l
 	if(check_prime(inp)):
 		l.append(inp)
 		
 
-for inp in l:
-	
+for inp in l: # iterate each prime of list l to check if it is circular prime
 	
 	temp=inp
 	temp=list(str(temp))
 	first_digit=list(temp.pop(0))
 	temp=temp+first_digit
+	temp1=temp
 
 	for i in temp:
 		st=st+str(i)
@@ -50,13 +53,16 @@ for inp in l:
 			
 	temp=int(st)
 	st=''
+
 	
-	while(inp!=temp):
+	while (inp!=temp):	
 		if(temp in l):
-					
+			if(temp1[0]=='0'):
+				break;	
 			temp=list(str(temp))	
 			first_digit=list(temp.pop(0))
 			temp=temp+first_digit
+			temp1=temp
 			for i in temp:
 				st=st+str(i)	
 			temp=int(st)
@@ -71,6 +77,5 @@ for inp in l:
 			
 
 print (circ)		
-
-
+print(len(circ))
 
